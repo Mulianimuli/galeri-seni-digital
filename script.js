a1ea504774fff05b1b9f14d0147dacca7992c8f1
Mulianimuli <muliani4523@gmail.com>
2025-11-15 11:28:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 localStorage.setItem('isLoggedIn', 'true');
                 loginMessage.textContent = 'Login berhasil! Mengalihkan...';
                 loginMessage.style.color = '#4CAF50';
-                window.location.href = 'dashboard.html';
+                window.location.href = 'Dashboard.html';
             } else {
                 loginMessage.textContent = 'Nama pengguna atau kata sandi salah.';
                 loginMessage.style.color = '#ee6c4d';
@@ -96,4 +96,5 @@ document.addEventListener('DOMContentLoaded', () => {
             switchContent('portfolio');
         }
     }
+
 });
